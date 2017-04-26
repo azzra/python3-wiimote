4f90f364c0c5d4684b577b35fc7b77932be3a9f2
tom <t.vitureau@purjus.fr>
2017-04-26 15:53:03 +0200

    clean setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup, Extension
 
-setup(name='cwiid',
-	version='0.6.00',
-	ext_modules=[Extension('cwiid', ['cwiidmodule.c', 'Wiimote.c'], libraries=['cwiid', 'bluetooth'])]
-	)
+setup(
+    name='cwiid',
+    version='3.0.0',
+    description='Python3 module for libcwiid',
+    author='Azzra',
+    author_email='azzra@users.noreply.github.com',
+    ext_modules=[Extension('cwiid', ['cwiidmodule.c', 'Wiimote.c'], libraries=['cwiid', 'bluetooth'])]
+)
